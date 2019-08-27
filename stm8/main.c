9a973f31ce0b0b75bc8d630200bb3684d215403d
Dmitry Latokhin <Latokhin@avp.ru>
2019-08-27 10:03:07 +0300

    msvc workspace added to stm8

--- stm8/main.c
+++ stm8/main.c
@@ -1,15 +1,13 @@
-/* MAIN.C file
- * 
- * Copyright (c) 2002-2005 STMicroelectronics
- */
 #include "stm8l15x.h"
 
 #define LED_BLUE 	GPIOC
-#define LED_GREEN GPIOE
+#define LED_GREEN 	GPIOE
 #define LED_PORT 	GPIO_Pin_7
 
 main()
 {
+	//https://github.com/hbendalibraham/stm8_started to help with mscode and sdcc
+	//just blinker code for discovery
 	GPIO_DeInit(LED_BLUE);
 	GPIO_Init(LED_BLUE, LED_PORT, GPIO_Mode_Out_PP_Low_Slow);
 	GPIO_DeInit(LED_GREEN);
